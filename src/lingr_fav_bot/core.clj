beb806dc1406deed39614eb58df40199e89f1e86
ujihisa <ujihisa@gmail.com>
2012-01-19 22:10:29 -0800

    lein new lingr-fav-bot

---
+++ src/lingr_fav_bot/core.clj
@@ -0,0 +1,1 @@
+(ns lingr-fav-bot.core)
---
+++ test/lingr_fav_bot/test/core.clj
@@ -0,0 +1,6 @@
+(ns lingr-fav-bot.test.core
+  (:use [lingr-fav-bot.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
