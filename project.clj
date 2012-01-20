a6741e5e55ede0d7e178c362b130ecba5fb9c594
ujihisa <ujihisa@gmail.com>
2012-01-19 22:13:07 -0800

    trivial echo bot

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject lingr-fav-bot "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :main lingr-fav-bot.core
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.2"]
+                 [compojure "1.0.0-RC2"]
+                 [ring "1.0.1"]])
--- src/lingr_fav_bot/core.clj
+++ src/lingr_fav_bot/core.clj
@@ -1,1 +1,15 @@
-(ns lingr-fav-bot.core)
+(ns lingr-clojure-bot.core
+  (:use
+    [clojure.data.json :only (read-json)]
+    [compojure.core]
+    [ring.adapter.jetty]))
+
+(defroutes hello
+  (GET "/" [] "working")
+  (POST "/"
+        {body :body}
+        (let [message (:message (first (:events (read-json (slurp body)))))]
+          (str message))))
+
+(defn -main []
+  (run-jetty hello {:port 80}))
